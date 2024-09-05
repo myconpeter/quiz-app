f7762fdee8b8947d725ad95e4e75bf538557e75a
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2024-09-05 09:00:26 +0100

    hmm

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
 
-import End from "./components/End";
-import Question from "./components/Question";
-import Start from "./components/Start";
-import quizData from "./data/quiz.json";
+import End from './components/End';
+import Question from './components/Question';
+import Start from './components/Start';
+import quizData from './data/quiz.json';
 
 let interval;
 
 const App = () => {
-  const dispatch = useDispatch();
-  const { step, answers } = useSelector((state) => state?.quizReducer);
-  console.log(step);
-  console.log(answers);
-  const [showModal, setShowModal] = useState(false);
-  const [time, setTime] = useState(0);
-  useEffect(() => {
-    if (step === 3) {
-      clearInterval(interval);
-    }
-  }, [step]);
+	const dispatch = useDispatch();
+	const { step, answers } = useSelector((state) => state?.quizReducer);
+	console.log(step);
+	console.log(answers);
+	const [showModal, setShowModal] = useState(false);
+	const [time, setTime] = useState(0);
+	useEffect(() => {
+		if (step === 3) {
+			clearInterval(interval);
+		}
+	}, [step]);
 
-  return (
-    <div className="App">
-      {step === 1 && <Start />}
-      {step === 2 && <Question />}
-      {step === 3 && (
-        <End
-          data={quizData.data}
-          time={time}
-          onAnswersCheck={() => setShowModal(true)}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className='App'>
+			{step === 1 && <Start />}
+			{step === 2 && <Question />}
+			{step === 3 && (
+				<End
+					data={quizData.data}
+					time={time}
+					onAnswersCheck={() => setShowModal(true)}
+				/>
+			)}
+
+			<div className='owner'>
+				<p>CHARLES ONYEDIKACHI ORIH</p>
+				<p>20/SC/CO/897</p>
+			</div>
+		</div>
+	);
 };
 
 export default App;
